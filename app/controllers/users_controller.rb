0d4219670a5e514b79c076c757cb90c815215a28
geraldgsh <geraldgsh@hotmail.com>
2019-11-13 03:15:29 +0800

    Generated sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                     :password_confirmation)
+      params.require(:user).permit(:username, :email, :password)
     end
 end
